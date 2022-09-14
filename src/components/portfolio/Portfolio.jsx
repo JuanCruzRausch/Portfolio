fedd8efb9f1392d81df716a1517ff577f121e5a2
JuanCruzRausch <84107721+JuanCruzRausch@users.noreply.github.com>
2022-09-14 11:40:15 -0300

    add posada to portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./portfolio.css";
 import IMG1 from "../../assets/project1.jpeg";
 import IMG2 from "../../assets/project2.jpeg";
+import IMG3 from "../../assets/project3.jpeg";
 import { BsGithub } from "react-icons/bs";
 import { RiPagesLine } from "react-icons/ri";
 
@@ -52,6 +53,30 @@ const Portfolio = () => {
               </a>
               <a
                 href="https://mercado-los-7-enanitos.vercel.app/"
+                className="btn btn-primary"
+                target="_blank"
+              >
+                <RiPagesLine className="icons-port" /> Live Demo
+              </a>
+            </div>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-img">
+            <img src={IMG3} alt="img3" />
+          </div>
+          <div className="noimg">
+            <h3>Posada Brillo de Luna</h3>
+            <div className="portfolio_item-cta">
+              <a
+                href="https://github.com/JuanCruzRausch/Posada-Brillo-De-Luna"
+                className="btn"
+                target="_blank"
+              >
+                <BsGithub className="icons-port" /> GitHub
+              </a>
+              <a
+                href="http://posadabrillodeluna.com.ar/"
                 className="btn btn-primary"
                 target="_blank"
               >
