b295281ac7a3efff74bb954f5d4483cd31564489
JuanCruzRausch <juancruz_rausch@hotmail.com>
2023-03-17 09:19:17 -0300

    actualizando cv español

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CVEng from "../../assets/CV ingles.pdf";
-import CVSpa from "../../assets/CVEspañol.pdf";
+import CVSpa from "../../assets/CV Español.pdf";
 
 const CTA = () => {
   return (
